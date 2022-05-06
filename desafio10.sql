aae242c8c632a8813989ed400641b0862584633d
Juliano Boese <juliano.boese@gmail.com>
2022-05-06 15:58:58 +0000

    feat(req10): :sparkles: show plays by song from specific subscriptions
    requirement 10 complete

---
+++ desafio10.sql
@@ -0,0 +1,8 @@
+SELECT s.song_name AS 'nome', COUNT(*) AS 'reproducoes'
+FROM SpotifyClone.songs s
+INNER JOIN SpotifyClone.plays p ON s.song_id = p.song_id
+INNER JOIN SpotifyClone.users u ON p.user_id = u.user_id
+INNER JOIN SpotifyClone.subscriptions sub ON u.subscription_id = sub.subscription_id
+WHERE sub.subscription_name IN ('gratuito', 'pessoal')
+GROUP BY s.song_id
+ORDER BY s.song_name;
