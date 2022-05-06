45f22f63af278418f5909ab5ca6496fa3205ae62
Juliano Boese <juliano.boese@gmail.com>
2022-05-06 14:26:43 +0000

    feat(req6): :sparkles: show subscription prices and revenue data
    requirement 6 complete

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT
+  CONVERT(ROUND(MIN(sub.subscription_price), 2), CHAR) AS 'faturamento_minimo',
+  CONVERT(ROUND(MAX(sub.subscription_price), 2), CHAR) AS 'faturamento_maximo',
+  CONVERT(ROUND(AVG(sub.subscription_price), 2), CHAR) AS 'faturamento_medio',
+  CONVERT(ROUND(SUM(sub.subscription_price), 2), CHAR) AS 'faturamento_total'
+FROM SpotifyClone.users u
+INNER JOIN subscriptions sub ON u.subscription_id = sub.subscription_id;
