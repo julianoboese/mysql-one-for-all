6603cb56db8aa1fe8833626759a320176e9a0449
Juliano Boese <juliano.boese@gmail.com>
2022-05-06 12:56:44 +0000

    feat(req3): :sparkles: show plays data from each user
    requirement 3 complete

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+SELECT
+  u.user_name AS 'usuario',
+  COUNT(*) AS 'qtde_musicas_ouvidas',
+  ROUND(SUM(s.song_length) / 60, 2) AS 'total_minutos'
+FROM SpotifyClone.users u
+INNER JOIN plays p ON u.user_id = p.user_id
+INNER JOIN songs s ON p.song_id = s.song_id
+GROUP BY u.user_name
+ORDER BY u.user_name;
