a63770a55b739adae5d98a40515386dbd49b683c
Juliano Boese <juliano.boese@gmail.com>
2022-05-06 16:24:20 +0000

    style: :art: add dabatase name where missing

--- desafio3.sql
+++ desafio3.sql
@@ -3,7 +3,7 @@ SELECT
   COUNT(*) AS 'qtde_musicas_ouvidas',
   ROUND(SUM(s.song_length) / 60, 2) AS 'total_minutos'
 FROM SpotifyClone.users u
-INNER JOIN plays p ON u.user_id = p.user_id
-INNER JOIN songs s ON p.song_id = s.song_id
+INNER JOIN SpotifyClone.plays p ON u.user_id = p.user_id
+INNER JOIN SpotifyClone.songs s ON p.song_id = s.song_id
 GROUP BY u.user_name
 ORDER BY u.user_name;
--- desafio6.sql
+++ desafio6.sql
@@ -4,4 +4,4 @@ SELECT
   CONVERT(ROUND(AVG(sub.subscription_price), 2), CHAR) AS 'faturamento_medio',
   CONVERT(ROUND(SUM(sub.subscription_price), 2), CHAR) AS 'faturamento_total'
 FROM SpotifyClone.users u
-INNER JOIN subscriptions sub ON u.subscription_id = sub.subscription_id;
+INNER JOIN SpotifyClone.subscriptions sub ON u.subscription_id = sub.subscription_id;
