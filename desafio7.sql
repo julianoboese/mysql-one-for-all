67ac82ebe06801ef6811ef4e64773b1b73b58cb3
Juliano Boese <juliano.boese@gmail.com>
2022-05-06 14:38:25 +0000

    feat(req7): :sparkles: show amount of followers by artist and album
    requirement 7 complete

---
+++ desafio7.sql
@@ -0,0 +1,6 @@
+SELECT ar.artist_name AS 'artista', al.album_name AS 'album', COUNT(*) AS 'seguidores'
+FROM SpotifyClone.artists ar
+INNER JOIN SpotifyClone.albums al ON ar.artist_id = al.artist_id
+INNER JOIN SpotifyClone.followers f ON ar.artist_id = f.artist_id
+GROUP BY al.album_id
+ORDER BY seguidores DESC, ar.artist_name, al.album_name;
